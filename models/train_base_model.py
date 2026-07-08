efbb7777b8e21f201f2dc90cf09540a22a69137f
Gilbert Horne Jr <gilhornejr@MacBookPro.lan>
2026-07-08 15:39:39 -0400

    Defined preprocessing steps for features

--- models/train_base_model.py
+++ models/train_base_model.py
@@ -11,16 +11,29 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import roc_auc_score, accuracy_score, recall_score, precision_score, f1_score, brier_score_loss
 
-
 #import dataset, convert to dataframe, print first 10 rows:
 raw_data_path = "data/raw/heart_failure_clinical_records_dataset.csv"
 df = pd.read_csv(raw_data_path)
 print(df.head(10))
 
 #split dataset into features and target varibles:
-X = df.drop(columns=["DEATH_EVENT"], axis=1)
+X = df.drop(columns=["DEATH_EVENT"])
 y = df["DEATH_EVENT"]
 
 #split dataset into train and test sets:
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, shuffle=True, stratify=y)
+
+#define features to be scaled an imputed: 
+numeric_features = X.columns.tolist()
+
+#define preprocessing steps for features:
+preprocessor = ColumnTransformer(
+    transformers=[
+            ('num', Pipeline(
+                [('imputer', SimpleImputer(strategy='median')),
+                 ('scaler', StandardScaler())
+                ]), numeric_features)
+    ]
+)
+
